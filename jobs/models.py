eeb981c6de32ad040f27cde166b9302bc2de25c1
sanya midha <sanyamidha.10@gmail.com>
2021-10-20 19:06:26 +0530

    Jobs app initialised

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+    imagesfun = models.ImageField(upload_to='image/', default='')
+    summary = models.CharField(max_length=200, default='')
