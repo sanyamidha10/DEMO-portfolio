a713d36f9c3b3c7f7d1b96537fba68bbe10e0b7f
sanya midha <sanyamidha.10@gmail.com>
2021-10-20 22:46:03 +0530

    Blog App initialised

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    Title = models.CharField(max_length=250, default='')
+    PublicationDate = models.DateTimeField(default='')
+    body = models.TextField(max_length=500, default='')
+    image = models.ImageField(upload_to='image/',default='')
